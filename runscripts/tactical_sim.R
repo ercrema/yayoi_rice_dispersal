269438246f74175830cf12eedbf1576003fe2032
Enrico R. Crema <enrico.crema@gmai.com>
2021-10-14 10:48:09 +0100

    Revised tactical simulation
    - Distances are now in great-arc
    - sim.constants containing all parameters stored in the R image file to
      facilitate posterior checks

--- runscripts/tactical_sim.R
+++ runscripts/tactical_sim.R
@@ -9,9 +9,7 @@ library(rcarbon)
 
 # Generate Spatial Window for Analyses ----
 sf_japan <- ne_states(country = "japan") |> subset(!name_vi %in%  c("Okinawa","Hokkaido"))
-win <- as(sf_japan, "SpatialPolygons") |>
- unionSpatialPolygons(IDs = rep(1, nrow(sf_japan))) |>
-  spTransform(CRSobj = CRS("+proj=utm +zone=54 ellps=WGS84"))
+win <- as(sf_japan, "SpatialPolygons") |>  unionSpatialPolygons(IDs = rep(1, nrow(sf_japan)))
 ## Exclude small islands
 win <- disaggregate(win) 
 win  <- win[order(raster::area(win),decreasing=TRUE)[1:3]]
@@ -23,12 +21,9 @@ set.seed(1323)
 Nsites = 150 #number of simulated sites
 sites <- spsample(win, n = Nsites, type = 'random') 
 
-# Compute Distance from Northern Kyushu Site
-origin_point = c(-520847.3,3751061)
-distance.tmp <- rbind(origin_point, coordinates(sites)) |> 
- dist() |> as.matrix()
-d <- distance.tmp[1, -1] / 1000
-dmat <- distance.tmp[-1,-1] / 1000
+dist_mat  <- spDists(sites,longlat=TRUE) #inter-site distance matrix
+origin_point  <- c(129.95798382,33.44851467) #Nabatake Site 
+dist_org  <-  spDistsN1(sites,origin_point,longlat=TRUE) #distance from nabatake site
 
 # Associate dates to sites
 Ndates = 400 #total number of dates
@@ -45,7 +40,7 @@ Ccov_GPL2 <- compileNimble(cov_GPL2)
 dispersalmodel <- nimbleCode({
 	for (k in 1:Nsites){
 		# Model
-		mu[k] <- beta0 + (s[k]+beta1)*d[k]
+		mu[k] <- beta0 + (s[k]+beta1)*dist_org[k]
 		alpha[k] ~ dnorm(mean=mu[k],sd=sigma)
 		delta[k] ~ dgamma(shape=omega,rate=phi)
 		alpha2[k] <- alpha[k] - delta[k] - 1 
@@ -53,23 +48,31 @@ dispersalmodel <- nimbleCode({
 	for (i in 1:Ndates){
 		Y[i] ~ dunif(min=alpha2[id.sites[i]],max=alpha[id.sites[i]]);
 	}
-	#fixed parameterts
-	beta0 <- 3000
-	omega <- 3
-	phi <- 0.01
-	beta1 <- -0.7
-	sigma <- 100
-	etasq <- 0.2
-	rhosq <- 0.0001
 	mu_s[1:Nsites] <- 0
-	cov_s[1:Nsites, 1:Nsites] <- cov_GPL2(dmat[1:Nsites, 1:Nsites], rhosq, etasq, 0.00001)
+	cov_s[1:Nsites, 1:Nsites] <- cov_GPL2(dist_mat[1:Nsites, 1:Nsites], rhosq, etasq, 0.00001)
 	s[1:Nsites] ~ dmnorm(mu_s[1:Nsites], cov = cov_s[1:Nsites, 1:Nsites])
 }) 
+
+## Define Parameters ----
+sim.constants  <- list()
+sim.constants$Ndates <- Ndates
+sim.constants$Nsites <- Nsites
+sim.constants$dist_org <- dist_org
+sim.constants$dist_mat <- dist_mat
+sim.constants$id.sites <- id.sites
+# Model parameters
+sim.constants$beta0 <- 3000
+sim.constants$beta1 <- -0.7
+sim.constants$sigma <- 100
+sim.constants$etasq <- 0.1
+sim.constants$rhosq <- 0.002
+sim.constants$omega <- 2
+sim.constants$phi <- 0.005
 
 
 ## Simulate Parameters ----
 set.seed(123)
-simModel <- nimbleModel(code = dispersalmodel,constants = list(Ndates = Ndates,d = d, dmat = dmat,Nsites = Nsites, id.sites = id.sites))
+simModel <- nimbleModel(code = dispersalmodel,constants = sim.constants)
                        
 simModel$simulate('s')
 simModel$simulate('mu')
@@ -96,8 +99,9 @@ for (i in 1:Nsites)
 
 
 # Save simulation output ----
-save(SiteInfo,DateInfo,win.sf,dmat,d,file=here('results','tactical_sim_res.RData'))
+save(sim.constants,SiteInfo,DateInfo,win,file=here('results','tactical_sim_res.RData'))
 
+# plot(sim.constants$dist_org,BPtoBCAD(SiteInfo$Earliest),pch=20)
 # ggplot(win.sf) + geom_sf() + geom_point(data=SiteInfo,aes(x=Easting,y=Northing,col=s)) + scale_color_gradient2()
 # ggplot(win.sf) + geom_sf() + geom_point(data=SiteInfo,aes(x=Easting,y=Northing,col=alpha)) + scale_color_gradient2(low = 'blue',mid ='white',high = 'red',midpoint = 2500)
 
