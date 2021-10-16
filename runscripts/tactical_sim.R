623f9b312548ee6602b947628712940b17679494
Enrico R. Crema <enrico.crema@gmai.com>
2021-10-16 13:59:23 +0100

    Revised tactical simulation
    - Previous model had unrealistic dispersal rates
    - Changed name of objects with prefix sim.
    - Exported sampling sites as sf class object

--- runscripts/tactical_sim.R
+++ runscripts/tactical_sim.R
@@ -9,21 +9,21 @@ library(rcarbon)
 
 # Generate Spatial Window for Analyses ----
 sf_japan <- ne_states(country = "japan") |> subset(!name_vi %in%  c("Okinawa","Hokkaido"))
-win <- as(sf_japan, "SpatialPolygons") |>  unionSpatialPolygons(IDs = rep(1, nrow(sf_japan)))
+sampling.win <- as(sf_japan, "SpatialPolygons") |>  unionSpatialPolygons(IDs = rep(1, nrow(sf_japan)))
 ## Exclude small islands
-win <- disaggregate(win) 
-win  <- win[order(raster::area(win),decreasing=TRUE)[1:3]]
-win.sf  <-  as(win,'sf')
+sampling.win <- disaggregate(sampling.win) 
+sampling.win  <- sampling.win[order(raster::area(sampling.win),decreasing=TRUE)[1:3]]
 
 # Tactical Simulation  ----
-## Simulate Site Locations and Distances ----
-set.seed(1323)
-Nsites = 150 #number of simulated sites
-sites <- spsample(win, n = Nsites, type = 'random') 
 
-dist_mat  <- spDists(sites,longlat=TRUE) #inter-site distance matrix
+## Simulate Site Locations and Distances ----
+set.seed(12345)
+Nsites = 150 #number of simulated sites
+sim.sites <- spsample(sampling.win, n = Nsites, type = 'random') 
+
+dist_mat  <- spDists(sim.sites,longlat=TRUE) #inter-site distance matrix
 origin_point  <- c(129.95798382,33.44851467) #Nabatake Site 
-dist_org  <-  spDistsN1(sites,origin_point,longlat=TRUE) #distance from nabatake site
+dist_org  <-  spDistsN1(sim.sites,origin_point,longlat=TRUE) #distance from nabatake site
 
 # Associate dates to sites
 Ndates = 400 #total number of dates
@@ -49,7 +49,7 @@ dispersalmodel <- nimbleCode({
 		Y[i] ~ dunif(min=alpha2[id.sites[i]],max=alpha[id.sites[i]]);
 	}
 	mu_s[1:Nsites] <- 0
-	cov_s[1:Nsites, 1:Nsites] <- cov_GPL2(dist_mat[1:Nsites, 1:Nsites], rhosq, etasq, 0.00001)
+	cov_s[1:Nsites, 1:Nsites] <- cov_GPL2(dist_mat[1:Nsites, 1:Nsites], rhosq, etasq, 0.000001)
 	s[1:Nsites] ~ dmnorm(mu_s[1:Nsites], cov = cov_s[1:Nsites, 1:Nsites])
 }) 
 
@@ -62,16 +62,18 @@ sim.constants$dist_mat <- dist_mat
 sim.constants$id.sites <- id.sites
 # Model parameters
 sim.constants$beta0 <- 3000
-sim.constants$beta1 <- -0.7
+sim.constants$beta1 <- -0.8
 sim.constants$sigma <- 100
-sim.constants$etasq <- 0.1
-sim.constants$rhosq <- 0.002
+sim.constants$etasq <- 0.04
+sim.constants$rhosq <- 0.00006
 sim.constants$omega <- 2
 sim.constants$phi <- 0.005
 
 
+
+
 ## Simulate Parameters ----
-set.seed(123)
+set.seed(12345)
 simModel <- nimbleModel(code = dispersalmodel,constants = sim.constants)
                        
 simModel$simulate('s')
@@ -82,27 +84,31 @@ simModel$simulate('alpha2')
 simModel$simulate('Y')
 
 ### Combine Results ----
-coords  <-  as.data.frame(coordinates(sites))
+coords  <-  as.data.frame(coordinates(sim.sites))
 dates <- data.frame(cra = round(uncalibrate(round(simModel$Y))$ccCRA), cra_error = 20)
 dates$med  <-  medCal(calibrate(dates$cra,dates$cra_error))
 
-DateInfo = data.frame(SiteID=id.sites,cra=dates$cra,cra_error=dates$cra_error,med=dates$med)
-SiteInfo = data.frame(SiteID=1:nrow(coords),Easting=coords$x,Northing=coords$y,Earliest=NA,Latest=NA,Diff=NA,alpha=simModel$alpha,s=simModel$s)
+sim.DateInfo = data.frame(SiteID=id.sites,cra=dates$cra,cra_error=dates$cra_error,med=dates$med)
+sim.SiteInfo = data.frame(SiteID=1:nrow(coords),Longitude=coords$x,Latitude=coords$y,Earliest=NA,Latest=NA,Diff=NA,alpha=simModel$alpha,s=simModel$s)
 
 for (i in 1:Nsites)
 {
-	tmp.dates <- subset(DateInfo,SiteID==SiteInfo$SiteID[i])
-	SiteInfo$Earliest[i] = max(tmp.dates$med)
-	SiteInfo$Latest[i] = min(tmp.dates$med)
-	SiteInfo$Diff[i] = SiteInfo$Earliest[i] - SiteInfo$Latest[i]
+	tmp.dates <- subset(sim.DateInfo,SiteID==sim.SiteInfo$SiteID[i])
+	sim.SiteInfo$Earliest[i] = max(tmp.dates$med)
+	sim.SiteInfo$Latest[i] = min(tmp.dates$med)
+	sim.SiteInfo$Diff[i] = sim.SiteInfo$Earliest[i] - sim.SiteInfo$Latest[i]
 }
 
 
+sim.sites <- as(sim.sites,'SpatialPointsDataFrame')
+sim.sites@data = data.frame(s=simModel$s)
+sim.sites@data$rate=-1/(sim.constants$beta1 + simModel$s)
+sim.sites@data$arrival = BPtoBCAD(sim.constants$beta0 + (sim.constants$beta1 + simModel$s) * sim.constants$dist_org)
+sim.sites <- as(sim.sites,'sf')
+
+# range(sim.sites$s)
+# range(sim.sites$rate)
+# range(sim.sites$arrival)
+
 # Save simulation output ----
-save(sim.constants,SiteInfo,DateInfo,win,file=here('results','tactical_sim_res.RData'))
-
-# plot(sim.constants$dist_org,BPtoBCAD(SiteInfo$Earliest),pch=20)
-# ggplot(win.sf) + geom_sf() + geom_point(data=SiteInfo,aes(x=Easting,y=Northing,col=s)) + scale_color_gradient2()
-# ggplot(win.sf) + geom_sf() + geom_point(data=SiteInfo,aes(x=Easting,y=Northing,col=alpha)) + scale_color_gradient2(low = 'blue',mid ='white',high = 'red',midpoint = 2500)
-
-
+save(sim.constants,sim.SiteInfo,sim.DateInfo,sim.sites,file=here('results','tactical_sim_res.RData'))
