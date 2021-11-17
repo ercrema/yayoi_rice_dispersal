0c98e23b0ffc412f7c1d0a73d0c8b8219f9ed998
Enrico R. Crema <enrico.crema@gmai.com>
2021-11-17 18:59:18 +0000

    Added tactical simulation for phase models

---
+++ runscripts/phasemodel_tactical.R
@@ -0,0 +1,91 @@
+#Load Libraries and Data ----
+library(here)
+library(coda)
+library(nimbleCarbon)
+library(rcarbon)
+load(here('results','tactical_sim_res2.RData'))
+
+# Model assuming independence of samples ----
+model1 <- nimbleCode({
+	for (i in 1:Ndates)
+	{
+		theta[i] ~ dunif(b,a);
+		# Calibration
+		mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]);
+		sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]);
+		error[i] <- (cra_error[i]^2+sigmaCurve[i]^2)^(1/2);
+		cra[i] ~ dnorm(mean=mu[i],sd=error[i]);
+	}
+	a ~ dunif(0,10000);
+	b ~ dunif(0,10000);
+	unif.const ~ dconstraint(b<a);
+})
+
+data("intcal20") 
+constants1 <- list(Ndates=sim.constants$Ndates,calBP=intcal20$CalBP,C14BP=intcal20$C14Age,C14err=intcal20$C14Age.sigma)
+d1 <- list(cra=d.sim$cra,cra_error=d.sim$cra_error,unif.const=1)
+theta.init = medCal(calibrate(d1$cra,d1$cra_error,verbose = FALSE))
+inits1 <- list(a=5000,b=500,theta=theta.init)
+
+#Run MCMC
+mcmc.samples1<- nimbleMCMC(code = model1,constants = constants1,data = d1,niter = 100000, nchains = 3, thin=1, nburnin = 50000,monitors=c('a','b','theta'), inits=inits1, samplesAsCodaMCMC=TRUE)
+
+#Diagnostics
+rhat1  <- gelman.diag(mcmc.samples1,multivariate = FALSE)
+ess1  <- effectiveSize(mcmc.samples1)
+
+
+
+
+# Model integrating sample interdependence ----
+model2 <- nimbleCode({
+	for (k in 1:Nsites)
+	{
+		delta[k] ~ dgamma(gamma1,(gamma1-1)/gamma2)
+		alpha[k] ~ dunif(max=a,min=b);
+	}
+
+	for (i in 1:Ndates){
+		theta[i] ~ dunif(alpha[id.sites[i]]-delta[id.sites[i]]+1,alpha[id.sites[i]]);
+		mu[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]);
+		sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]);
+		error[i] <- (cra_error[i]^2+sigmaCurve[i]^2)^(1/2);
+		cra[i] ~ dnorm(mean=mu[i],sd=error[i]);
+	}
+
+	# Set Prior for Duration
+	a ~ dunif(0,10000);
+	b ~ dunif(0,10000);	unif.const ~ dconstraint(b<a);
+	gamma1 ~ dunif(1,20);
+	gamma2 ~ T(dnorm(mean=200,sd=100),1,500);
+})
+
+data("intcal20") 
+constants2 <- list(Ndates=sim.constants$Ndates,
+		   Nsites=sim.constants$Nsites,
+		   calBP=intcal20$CalBP,
+		   C14BP=intcal20$C14Age,
+		   C14err=intcal20$C14Age.sigma,
+		   id.sites=sim.constants$id.sites)
+d2 <- list(cra=d.sim$cra,cra_error=d.sim$cra_error,unif.const=1)
+theta.init  <-  medCal(calibrate(d1$cra,d1$cra_error,verbose = FALSE))
+dd  <-  data.frame(theta=theta.init,id=constants2$id.sites)
+buffer  <- 100
+earliest  <- aggregate(theta~id,max,data=dd)
+latest  <- aggregate(theta~id,min,data=dd)
+diff.age  <- earliest$theta - latest$theta
+delta.init  <- diff.age + buffer
+alpha.init  <- earliest$theta + buffer/2
+
+
+inits2 <- list(a=5000,b=500,gamma1=5,gamma2=200,theta=theta.init,alpha=alpha.init,delta=delta.init)
+
+#Run MCMC
+mcmc.samples2<- nimbleMCMC(code = model2,constants = constants2,data = d2,niter = 100000, nchains = 3, thin=1, nburnin = 50000,monitors=c('a','b','theta','gamma1','gamma2'), inits=inits2, samplesAsCodaMCMC=TRUE)
+
+#Diagnostics
+rhat2  <- gelman.diag(mcmc.samples2,multivariate = FALSE)
+ess2  <- effectiveSize(mcmc.samples2)
+
+# Save output ----
+save(mcmc.samples1,rhat1,ess1,mcmc.samples2,rhat2,ess2,file=here('results','res_tactical2.RData'))
---
+++ runscripts/tactical_sim2.R
@@ -0,0 +1,41 @@
+library(rcarbon)
+library(nimbleCarbon)
+library(here)
+
+set.seed(123)
+Nsites <- 10
+Ndates <- 30
+id.sites <- c(1:Nsites,sample(1:Nsites,size=Ndates-Nsites,replace=TRUE,prob=dexp(1:Nsites,rate=1)/sum(dexp(1:Nsites,rate=1))))
+
+sim.model <- nimbleCode({
+	for (k in 1:Nsites)
+	{
+		delta[k] ~ dgamma(5,(5-1)/200);
+		alpha[k] ~ dunif(max=a,min=b);
+		beta[k] <- alpha[k] - delta[k] + 1;
+	}
+
+	for (i in 1:Ndates){
+		theta[i] ~ dunif(beta[id.sites[i]],alpha[id.sites[i]]);
+	}
+})
+
+sim.constants <- list()
+sim.constants$Nsites <- Nsites
+sim.constants$Ndates  <- Ndates
+sim.constants$id.sites  <- id.sites
+sim.constants$a <- 3000
+sim.constants$b <- 2500
+
+set.seed(123)
+simModel <- nimbleModel(code = sim.model,constants = sim.constants)
+simModel$simulate('delta')
+simModel$simulate('alpha')
+simModel$simulate('beta')
+simModel$simulate('theta')
+
+# Combine and store output
+cra = uncalibrate(round(simModel$theta))$rCRA
+cra_error = rep(20,length(cra))
+d.sim <- list(cra=cra,cra_error=cra_error,id.sites=id.sites)
+save(d.sim,sim.constants,file=here('results','tactical_sim_res2.RData'))
