6e52259cf8685871b7aa694446b0397caddf530c
Enrico R. Crema <enrico.crema@gmai.com>
2022-01-21 18:02:39 +0000

    Revised version of tactical simulation

--- runscripts/gpqr_tactical.R
+++ runscripts/gpqr_tactical.R
@@ -3,43 +3,43 @@ library(nimbleCarbon)
 library(rcarbon)
 library(here)
 library(parallel)
+library(sf)
+library(maptools)
 library(coda)
 
 ## Data Setup ----
 # Load Simulated Data
-load(here('results','tactical_sim_res.RData'))
-
-# Data
-dat  <- list(cra=sim.DateInfo$cra,cra_error=sim.DateInfo$cra_error)
+load(here('data','tactical_sim_gpqr.RData'))
 
 # Constants
 data(intcal20)
-Ndates <- nrow(sim.DateInfo)
-Nsites  <- nrow(sim.SiteInfo)
-constants  <- list(Nsites=Nsites,Ndates=Ndates)
-constants$id.sites  <- sim.DateInfo$SiteID
-constants$dmat  <- sim.constants$dist_mat
-constants$d  <- sim.constants$dist_org
+constants  <- list()
+constants$N <- true.param$n
+constants$dist_mat  <- spDists(as_Spatial(sim.sites),longlat=TRUE)
+constants$dist_org  <- spDistsN1(as_Spatial(sim.sites),true.param$origin.point,longlat=TRUE)
 constants$calBP  <- intcal20$CalBP
 constants$C14BP  <- intcal20$C14Age
 constants$C14err  <- intcal20$C14Age.sigma
 constants$tau  <- 0.90
 
-# Fixed Inits
-buffer <- 100
-theta.init <- sim.DateInfo$med
-delta.init <- sim.SiteInfo$Diff + buffer
-alpha.init <- sim.SiteInfo$Earliest + buffer/2
+# Data
+dat  <- list()
+dat$cra  <- sim.sites$cra
+dat$cra_error  <- sim.sites$cra.error
 
+# Theta Init
+theta.init  <-  sim.sites$med.date
 
 # MCMC Function ----
-runFun  <- function(seed,dat,constants,theta.init,delta.init,alpha.init,niter,nburnin,thin)
+runFun  <- function(seed,dat,theta.init,constants,niter,nburnin,thin)
 {
-	library(nimbleCarbon) 
+	library(nimbleCarbon)
 	library(truncnorm)
+	library(cascsim)
+
 	# Variance Covariance Function	
-	cov_GPL2 <- nimbleFunction(
-				   run = function(dists = double(2), rhosq = double(0), etasq = double(0), sigmasq = double(0)) {
+	cov_ExpQ <- nimbleFunction(run = function(dists = double(2), rho = double(0), etasq = double(0),sigmasq = double(0)) 
+				   {
 					   returnType(double(2))
 					   n <- dim(dists)[1]
 					   result <- matrix(nrow = n, ncol = n, init = FALSE)
@@ -47,22 +47,22 @@ runFun  <- function(seed,dat,constants,theta.init,delta.init,alpha.init,niter,nb
 					   diag(deltaij) <- 1
 					   for(i in 1:n)
 						   for(j in 1:n)
-							   result[i, j] <- etasq*exp(-rhosq*dists[i,j]^2)+sigmasq*deltaij[i,j]
+							   result[i, j] <- etasq*exp(-0.5*(dists[i,j]/rho)^2) + sigmasq*deltaij[i,j]
 					   return(result)
 				   })
-	Ccov_GPL2 <- compileNimble(cov_GPL2)
-	assign('cov_GPL2',cov_GPL2,envir=.GlobalEnv)
+	Ccov_ExpQ <- compileNimble(cov_ExpQ)
+	assign('cov_ExpQ',cov_ExpQ,envir=.GlobalEnv)
 
+	# Handle constraints on dipsersal rate
+	dat$lim  <- rep(1,constants$N)
 	# Core model
-	gpqr <- nimbleCode({
-		for (k in 1:Nsites){
+	model <- nimbleCode({
+		for (i in 1:N){
 			# Model
-			mu[k] <- beta0 + (s[k]-beta1)*d[k]
-			alpha[k] ~ dAsymLaplace(mu=mu[k],sigma=sigma,tau=tau)
-			delta[k] ~ dgamma(shape=omega,rate=phi)
-		}
-		for (i in 1:Ndates){
-			theta[i] ~ dunif(alpha[id.sites[i]]-delta[id.sites[i]]+1,alpha[id.sites[i]]);
+			rate[i] <- -1/(s[i]-beta1)
+# 			lim[i] ~ dconstraint(rate[i]>0)
+			mu[i] <- beta0 + (s[i]-beta1)*dist_org[i]
+			theta[i] ~ dAsymLaplace(mu=mu[i],sigma=sigma,tau=tau)
 			mu.date[i] <- interpLin(z=theta[i], x=calBP[], y=C14BP[]);
 			sigmaCurve[i] <- interpLin(z=theta[i], x=calBP[], y=C14err[]);
 			sd[i] <- (cra_error[i]^2+sigmaCurve[i]^2)^(1/2);
@@ -70,46 +70,38 @@ runFun  <- function(seed,dat,constants,theta.init,delta.init,alpha.init,niter,nb
 		}
 		#priors
 		beta0 ~ dnorm(3000,sd=200);
-		beta1 ~ dexp(1);
+		beta1 ~ dexp(0.5)
 		sigma ~ dexp(0.01)
-		etasq ~ dexp(10);
-		rhosq ~ dexp(1000);
-		omega ~ T(dnorm(mean=3,sd=1),0,)
-		phi ~ T(dnorm(mean=0.01,sd=0.01),0,)
-		mu_s[1:Nsites] <- 0;
-		cov_s[1:Nsites, 1:Nsites] <- cov_GPL2(dmat[1:Nsites, 1:Nsites], rhosq, etasq, 0.00001)
-		s[1:Nsites] ~ dmnorm(mu_s[1:Nsites], cov = cov_s[1:Nsites, 1:Nsites])
+		etasq ~ dexp(20);
+		rho ~ T(dgamma(10,(10-1)/150),1,1350); #mode 150
+		mu_s[1:N] <- 0;
+		cov_s[1:N, 1:N] <- cov_ExpQ(dist_mat[1:N, 1:N], rho, etasq, 0.000001)
+		s[1:N] ~ dmnorm(mu_s[1:N], cov = cov_s[1:N, 1:N])
 	}) 
 
 	# MCMC initialisation
 	set.seed(seed)
-	inits  <-  list(delta=delta.init,alpha=alpha.init)
+	inits  <-  list()
 	inits$theta  <- theta.init
 	inits$beta0 <- rnorm(1,3000,200)
-	inits$beta1 <- rexp(1,1)
+	inits$beta1 <- rexp(1,0.5)
 	inits$sigma  <- rexp(1,0.01)
-	inits$rhosq  <- rexp(1,1000)
-	inits$etasq  <- rexp(1,10)
-	inits$omega  <- rtruncnorm(1,mean=3,sd=1,a=0)
-	inits$phi  <- rtruncnorm(1,mean=0.01,sd=0.01,a=0)
-	inits$s  <- rep(0,constants$Nsites)
-	inits$cov_s <- Ccov_GPL2(constants$dmat, inits$rhosq, inits$etasq, 0.0001)
-	inits$s <-  t(chol(inits$cov_s)) %*% rnorm(constants$Nsites)
+	inits$rho  <- rtgamma(1,shape=10,scale=(10-1)/200,min=1,max=1350)
+	inits$etasq  <- rexp(1,20)
+	inits$s  <- rep(0,constants$N)
+	inits$cov_s <- Ccov_ExpQ(constants$dist_mat, inits$rho, inits$etasq, 0.000001)
+	inits$s <-  t(chol(inits$cov_s)) %*% rnorm(constants$N)
 	inits$s <- inits$s[ , 1]  # so can give nimble a vector rather than one-column matrix
 
 	# Model Compilation
-	model.gpqr <- nimbleModel(gpqr, constants = constants,data=dat,inits=inits)
+	model.gpqr <- nimbleModel(model,constants = constants,data=dat,inits=inits)
 	cModel.gpqr <- compileNimble(model.gpqr)
 
 	# MCMC configuration
 	conf.gpqr <- configureMCMC(model.gpqr)
 	conf.gpqr$addMonitors('s')
-	conf.gpqr$addMonitors('rhosq')
+	conf.gpqr$addMonitors('rho')
 	conf.gpqr$addMonitors('etasq')
-	conf.gpqr$addMonitors('alpha')
-	conf.gpqr$addMonitors('delta')
-	conf.gpqr$addMonitors('omega')
-	conf.gpqr$addMonitors('phi')
 	conf.gpqr$removeSamplers('s[1:150]')
 	conf.gpqr$removeSamplers('beta1')
 	conf.gpqr$addSampler(c('beta1','s[1:150]'), type='AF_slice') 
@@ -121,21 +113,24 @@ runFun  <- function(seed,dat,constants,theta.init,delta.init,alpha.init,niter,nb
 	return(results)
 }
 
-# Setup and Execute MCMC in Parallel  ----
-ncores <- 3
+
+## Setup and Execution of MCMC in Parallel ----
+ncores <- 4
 cl <- makeCluster(ncores)
 # Run the model in parallel:
-seeds <- c(12, 34, 56)
-niter = 1500000
-nburnin = 500000
-thin = 100
-chain_output <- parLapply(cl = cl, X = seeds, fun = runFun, dat = dat,
-constants = constants, theta = theta.init, alpha.init = alpha.init, delta.init = delta.init, niter = niter, nburnin = nburnin,thin = thin)
+seeds <- c(12,45,67,89)
+niter = 8000000
+nburnin = 4000000
+thin = 400
+chain_output <- parLapply(cl = cl, X = seeds, fun = runFun, dat = dat,constants = constants, theta = theta.init, niter = niter, nburnin = nburnin,thin = thin)
 stopCluster(cl)
 # Convert into a mcmc.list object for diagnostic (see below)
-res <- mcmc.list(chain_output)
-rhat <- gelman.diag(res,multivariate = F);range(rhat$psrf[,1])
-ess <- effectiveSize(res) ;range(ess)
+gpqr_tactsim <- coda::mcmc.list(chain_output)
+rhat <- coda::gelman.diag(gpqr_tactsim,multivariate = F)
+range(rhat$psrf[,1])
+ii  <- which(rhat$psrf[,1]>1.01)  
+ess  <- coda::effectiveSize(gpqr_tactsim)
+range(ess)
 
 ## Store Output ----
-save(res,rhat,ess,file=here('results','res_tactical.RData'))
+save(gpqr_tactsim,file=here('results','gpqr_tactsim.RData'))
---
+++ runscripts/tactical_gpqr_sim.R
@@ -0,0 +1,37 @@
+# Load Libraries ----
+library(here)
+library(nimbleCarbon)
+library(rnaturalearth)
+library(sp)
+library(maptools)
+library(sf)
+library(ggplot2)
+library(viridis)
+library(rgeos)
+library(rcarbon)
+source(here('src','gpqrSim.R'))
+
+# Generate Spatial Window for Analyses ----
+sf_japan <- ne_states(country = "japan") |> subset(!name_vi %in%  c("Okinawa","Hokkaido"))
+sampling.win <- as(sf_japan, "SpatialPolygons") |>  unionSpatialPolygons(IDs = rep(1, nrow(sf_japan)))
+## Exclude small islands
+sampling.win <- disaggregate(sampling.win) 
+sampling.win  <- sampling.win[order(raster::area(sampling.win),decreasing=TRUE)[1:3]]
+
+# Target Parameters ----
+true.param  <- list()
+true.param$n  <- 150 #number of sites & dates
+true.param$origin.point <- c(129.95,33.45) #dispersal origin point
+true.param$beta0 <- 3000 #mean date at origin point
+true.param$beta1 <- 0.6 #reciprocal of dispersal rate
+true.param$sigma <- 100 #variability of arrival date
+true.param$etasq <- 0.08 #variability of dispersal rate
+true.param$rho <- 150 #range of spatial autocorrelation
+true.param$seed <- 13233 #random seed
+
+# Simulate Data ----
+sim.sites  <- gpqrSim(win=sampling.win,n=true.param$n,beta0=true.param$beta0,beta1=true.param$beta1,sigma=true.param$sigma,origin.point=true.param$origin.point,etasq=true.param$etasq,rho=true.param$rho,seed=true.param$seed)
+
+# Process output for Bayesian Analyses ----
+# Save simulation output ----
+save(true.param,sim.sites,file=here('data','tactical_sim_gpqr.RData'))
